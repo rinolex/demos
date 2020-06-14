bf987b786807fb4370d775f6494201d90a1010ad
rinolex <66743538+rinolex@users.noreply.github.com>
2020-06-14 18:10:31 +0300

    Unit Test Comments

--- HRManagementSystem/src/test/java/com/HRManagementSystem/HrManagementSystemApplicationTests.java
+++ HRManagementSystem/src/test/java/com/HRManagementSystem/HrManagementSystemApplicationTests.java
@@ -3,6 +3,12 @@ package com.HRManagementSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Unit Tests
+ * 
+ * @author rino
+ *
+ */
 @SpringBootTest
 class HrManagementSystemApplicationTests {
 
